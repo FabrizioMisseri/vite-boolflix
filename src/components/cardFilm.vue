78919be1777408314341e9579b90ba29844d4416
FabrizioMisseri <misserifabrizio@gmail.com>
2022-11-14 19:39:24 +0100

    add flags

--- src/components/cardFilm.vue
+++ src/components/cardFilm.vue
@@ -5,6 +5,7 @@ export default {
     props: {
         film: Object,
     },
+
 }
 </script>
 
@@ -14,7 +15,29 @@ export default {
         <ul>
             <li> titolo: {{ film.title }} </li>
             <li>Titolo Originale: {{ film.original_title }} </li>
-            <li>Lingua: {{ film.original_language }} </li>
+
+            <li>
+                <div v-if="film.original_language === 'it'">
+                    bandiera italia
+                </div>
+                <div v-if="film.original_language === 'en'">
+                    bandiera inglese
+                </div>
+                <div v-if="film.original_language === 'de'">
+                    bandiera germania
+                </div>
+                <div v-if="film.original_language === 'ja'">
+                    bandiera giapponese
+                </div>
+                <div v-if="film.original_language !== 'it' &&
+                    film.original_language !== 'en' &&
+                    film.original_language !== 'de' &&
+                    film.original_language !== 'ja'
+                ">
+                    Lingua: {{ film.original_language }}
+                </div>
+            </li>
+
             <li>Voto: {{ Math.floor(film.vote_average / 2) }} </li>
         </ul>
 
--- src/components/cardSerie.vue
+++ src/components/cardSerie.vue
@@ -14,7 +14,27 @@ export default {
         <ul>
             <li> titolo: {{ serie.name }} </li>
             <li>Titolo Originale: {{ serie.original_name }} </li>
-            <li>Lingua: {{ serie.original_language }} </li>
+            <li>
+                <div v-if="serie.original_language === 'it'">
+                    bandiera italia
+                </div>
+                <div v-if="serie.original_language === 'en'">
+                    bandiera inglese
+                </div>
+                <div v-if="serie.original_language === 'de'">
+                    bandiera germania
+                </div>
+                <div v-if="serie.original_language === 'ja'">
+                    bandiera giapponese
+                </div>
+                <div v-if="serie.original_language !== 'it' &&
+                    serie.original_language !== 'en' &&
+                    serie.original_language !== 'de' &&
+                    serie.original_language !== 'ja'
+                ">
+                    Lingua: {{ serie.original_language }}
+                </div>
+            </li>
             <li>Voto: {{ Math.floor(serie.vote_average / 2) }} </li>
         </ul>
 
